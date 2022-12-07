e4abe03cdabf05b5609ee110b0a957da570e7a91
RENNES\b.carion <baptiste.carion@sfr.fr>
2022-12-07 12:02:06 +0100

    Maj 07/12 12:01 : Many things changed but I need to go eat sry

---
+++ Assets/Scripts/GamePuzzleSceneScripts/PieceGatherer.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PieceGatherer : MonoBehaviour
+{
+    public GamePuzzleSceneMovePiece currentHittedPiece;
+    public List<GamePuzzleSceneMovePiece.PositionPiece> grillePosOccupied;
+    private List<GamePuzzleSceneMovePiece.PositionPiece> tempoGrillePos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        grillePosOccupied = new List<GamePuzzleSceneMovePiece.PositionPiece>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+        if (Input.touchCount >= 1)
+        {
+            var tempPosition = Input.touches[0].position;
+            var screenPos = new Vector3(tempPosition.x, tempPosition.y, Camera.main.nearClipPlane - Camera.main.transform.position.z);
+
+            if (currentHittedPiece == null)
+            {
+                var ray = Camera.main.ScreenPointToRay(screenPos);
+                var hit = Physics2D.Raycast(ray.origin, ray.direction);
+                if (hit == true) 
+                {
+                    currentHittedPiece = hit.collider.GetComponent<GamePuzzleSceneMovePiece>();
+                    tempoGrillePos = currentHittedPiece.ReturnOccupiedPos();
+                    foreach (GamePuzzleSceneMovePiece.PositionPiece item in tempoGrillePos)
+                    {
+                        grillePosOccupied.Remove(item);
+                    }
+
+                }
+            }
+
+            if (currentHittedPiece != null)
+            {
+                var newPos = Camera.main.ScreenToWorldPoint(screenPos);
+                currentHittedPiece.OnTouch(newPos);
+            }
+        }
+        else
+        {
+            if (currentHittedPiece != null)
+            {
+                currentHittedPiece.OnPasTouch();
+                tempoGrillePos = currentHittedPiece.ReturnOccupiedPos();
+                foreach (GamePuzzleSceneMovePiece.PositionPiece item in tempoGrillePos)
+                {
+                    grillePosOccupied.Add(item);
+                }
+                currentHittedPiece = null;
+            }
+        }
+        
+
+    }
+}
