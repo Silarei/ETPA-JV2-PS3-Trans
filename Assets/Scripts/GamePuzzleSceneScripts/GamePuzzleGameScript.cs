6d50125be3f6044f62e460d04540f9108aa192e5
CARION Baptiste <baptiste.carion@sfr.fr>
2022-12-12 09:31:12 +0100

    Maj 9/12: Ready for trans (pushed late as I'm a dumbass)

--- Assets/Scripts/GamePuzzleSceneScripts/GamePuzzleGameScript.cs
+++ Assets/Scripts/GamePuzzleSceneScripts/GamePuzzleGameScript.cs
@@ -1,10 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class GamePuzzleGameScript : MonoBehaviour
 {
     public PieceGatherer pieceGatherer;
+
+    public float currentTime;
+    public TMP_Text timer;
+    private float end;
 
     public GameObject saxoPiece;
     public GameObject pianoPiece;
@@ -28,15 +34,16 @@ public class GamePuzzleGameScript : MonoBehaviour
     void Start()
     {
         victory = false;
+        end = 0f;
         var aleaLevel = Random.Range(0, 1);
         if (aleaLevel <= 0.5f)
         {
             piece1 = Instantiate(batteriePiece90);
-            piece2 = Instantiate(batteriePiece);
-            piece3 = Instantiate(microPiece);
-            piece4 = Instantiate(guitarePiece90);
+            piece2 = Instantiate(batteriePiece, new Vector3(0.7f,0f,0), Quaternion.identity);
+            piece3 = Instantiate(microPiece, new Vector3(1.7f,-3.7f,0), Quaternion.identity);
+            piece4 = Instantiate(guitarePiece90, new Vector3(1.75f,-1.6f,0), guitarePiece90.transform.rotation);
             piece5 = Instantiate(guitarePiece90);
-            piece6 = Instantiate(hornPiece270);
+            piece6 = Instantiate(hornPiece270, new Vector3(-0.7f,-3.6f,0), hornPiece270.transform.rotation);
         }
         else if (aleaLevel > 0.5f)
         {
@@ -52,15 +59,20 @@ public class GamePuzzleGameScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        currentTime += Time.deltaTime;
+
+        if (!victory) {
+            timer.text = "" + System.Math.Round(currentTime, 2);
+        }
         if (pieceGatherer.grillePosOccupied.Count == 16 && !victory)
         {
+            end = currentTime;
             Debug.Log("Victoire !");
             victory = true;
         }
-        if (pieceGatherer.grillePosOccupied.Count != 16 && victory)
+        if (currentTime - end > 10 && victory)
         {
-            Debug.Log("Ah non");
-            victory = false;
+            SceneManager.LoadScene("MenuFreeGame");
         }
     }
 }
